30b4bdb9d443e7a96fe437a8b5f47f0f9b1bcdf7
xupp <390241457@qq.com>
2017-04-28 13:15:35 +0800

    second commit

---
+++ public/index111.php
@@ -0,0 +1,56 @@
+<?php
+require_once __DIR__ . '/lib/WxPay.Api.php';
+
+class WXPay {
+    function index() {
+        //         初始化值对象
+        $input = new WxPayUnifiedOrder();
+        //         文档提及的参数规范：商家名称-销售商品类目
+        $input->SetBody("灵动商城-手机");
+        //         订单号应该是由小程序端传给服务端的，在用户下单时即生成，demo中取值是一个生成的时间戳
+        $input->SetOut_trade_no(time().'');
+        //         费用应该是由小程序端传给服务端的，在用户下单时告知服务端应付金额，demo中取值是1，即1分钱
+        $input->SetTotal_fee("1");
+        $input->SetNotify_url("http://paysdk.weixin.qq.com/example/notify.php");
+        $input->SetTrade_type("JSAPI");
+        //         由小程序端传给服务端
+        $input->SetOpenid($this->getSession()->openid);
+        //         向微信统一下单，并返回order，它是一个array数组
+        $order = WxPayApi::unifiedOrder($input);
+        //         json化返回给小程序端
+        header("Content-Type: application/json");
+        echo $this->getJsApiParameters($order);
+    }
+
+    private function getJsApiParameters($UnifiedOrderResult)
+    {
+        if(!array_key_exists("appid", $UnifiedOrderResult)
+        || !array_key_exists("prepay_id", $UnifiedOrderResult)
+        || $UnifiedOrderResult['prepay_id'] == "")
+        {
+            throw new WxPayException("参数错误");
+        }
+        $jsapi = new WxPayJsApiPay();
+        $jsapi->SetAppid($UnifiedOrderResult["appid"]);
+        $timeStamp = time();
+        $jsapi->SetTimeStamp("$timeStamp");
+        $jsapi->SetNonceStr(WxPayApi::getNonceStr());
+        $jsapi->SetPackage("prepay_id=" . $UnifiedOrderResult['prepay_id']);
+        $jsapi->SetSignType("MD5");
+        $jsapi->SetPaySign($jsapi->MakeSign());
+        $parameters = json_encode($jsapi->GetValues());
+        return $parameters;
+    }
+
+    private function getSession() {die;
+        $input = file_get_contents('php://input');
+        $data = explode('=',$input);
+        $code = $data[1];var_dump($code);
+        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.WxPayConfig::APPID.'&secret='.WxPayConfig::APPSECRET.'&js_code='.$code.'&grant_type=authorization_code';
+        $response = json_decode(file_get_contents($url));var_dump($response);die;
+        return $response;
+    }
+}
+
+$WxPay = new WXPay();
+$WxPay->index();

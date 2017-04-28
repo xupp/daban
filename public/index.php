f98011fb70993b3595f1eb77082998e5e1ec7d0e
xupp <390241457@qq.com>
2017-04-28 13:34:42 +0800

    four commit

--- public/index.php
+++ public/index.php
@@ -1,10 +1,17 @@
 <?php
-require '../vendor/autoload.php';
 
-use \LeanCloud\Client;
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use LeanCloud\Client;
+use LeanCloud\Engine\LeanEngine;
+use LeanCloud\Engine\Cloud;
 
 Client::initialize(
     '6LXne5XRbSQLmG3rwLmC9TdM-gzGzoHsz',
-    'xEhlLPpmOAKCNM4TnpTdsvEX'
+    'xEhlLPpmOAKCNM4TnpTdsvEX',
+    'rl2BJdmke7Q5kXMgFSIV1AIW'
 );
-echo 'success';
+
+
+$engine = new LeanEngine();
+$engine->start();
